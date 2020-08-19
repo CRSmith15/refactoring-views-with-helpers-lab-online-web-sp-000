be620745b9b45657fef40aa27dfdb6d714918060
Chance Smith <chancesmith97@hotmail.com>
2020-08-19 19:02:03 +0000

    Automatically backed up by Learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,6 +1,9 @@
 module ArtistsHelper
   def display_artist(artist)
     if @song.artist_id.exists?
-      link_to @song.artist.name, @artist
+      link_to @song.artist_name, artist_path(song.artist)
+    else 
+      link_to "Add Artist", edit_song_path(song)
+    end
   end
 end
